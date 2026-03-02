1cb4744ae45a0d6dce39dd5b390415b1c585069b
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-03-02 19:28:14 +0430

    refactor: move state and toggle to themeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-import { useState } from "react"
+
 import Header from "./components/Header"
 import Main from "./components/main"
-import { ThemeContext } from "./ThemeContext"
+import { ThemeProvider } from "./context/ThemeContext"
 
 
 function App() {
- const [theme, setTheme] = useState("light")
-
- const toggleTheme = () => {
-  setTheme(theme === "light" ? "dark" : "light")
- }
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeProvider>
      <Header />
      <Main />
-    </ThemeContext.Provider>
+    </ThemeProvider>
   )
 }
 
