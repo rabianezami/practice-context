bb61516f0b69dd875e4959013172bc9b06309015
rabia zia nezami <rabianezami786@gail.com>
2026-03-05 19:20:07 +0430

    add AuthProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 
 import Header from "./components/Header"
 import Main from "./components/main"
+import { AuthProvider } from "./context/AuthContext"
 import { ThemeProvider } from "./context/ThemeContext"
+import Login from "./components/Login"
 
 
 function App() {
 
   return (
-    <ThemeProvider>
-     <Header />
-     <Main />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <Header /> 
+        <Main />
+        <Login />
+      </ThemeProvider>
+    </AuthProvider>
   )
 }
 
